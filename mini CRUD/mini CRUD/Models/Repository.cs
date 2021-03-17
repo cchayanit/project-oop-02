57a3773ba7aa17442bce2f19a2dac5244743b899
cchayanit <chayanit.pm@gmail.com>
2021-03-17 22:44:48 +0700

    Create Employee Model

---
+++ mini CRUD/mini CRUD/Models/Repository.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace mini_CRUD.Models
+{
+    public static class Repository
+    {
+        private static List<Employee> allEmpoyees = new List<Employee>();
+        public static IEnumerable<Employee> AllEmpoyees
+        {
+            get { return allEmpoyees; }
+        }
+        public static void Create(Employee employee)
+        {
+            allEmpoyees.Add(employee);
+        }
+    }
+}
