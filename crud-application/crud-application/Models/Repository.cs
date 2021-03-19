36f93b55fcc9180f971e7eeb074681596aad5fb1
cchayanit <chayanit.pm@gmail.com>
2021-03-19 21:30:58 +0700

    Create Student Model

---
+++ crud-application/crud-application/Models/Repository.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace crud_application.Models
+{
+    public static class Repository
+    {
+        private static List<Student> allStudents = new List<Student>();
+        public static IEnumerable<Student> AllEmpoyees
+        {
+            get { return allStudents; }
+        }
+        public static void Create(Student employee)
+        {
+            allStudents.Add(employee);
+        }
+    }
+}
---
+++ crud-application/crud-application/Models/Student.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace crud_application.Models
+{
+    public class Student
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public int Year { get; set; }
+        public string Major { get; set; }
+        public Char Sex { get; set; }
+    }
+}
