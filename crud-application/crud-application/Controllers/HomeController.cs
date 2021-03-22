f0478b0e7d5af1531a7812dd5b3e7737f12a315b
cchayanit <chayanit.pm@gmail.com>
2021-03-22 12:58:32 +0700

    Edit form

--- crud-application/crud-application/Controllers/HomeController.cs
+++ crud-application/crud-application/Controllers/HomeController.cs
@@ -11,13 +11,7 @@ namespace crud_application.Controllers
 {
     public class HomeController : Controller
     {
-        /*private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
-        {
-            _logger = logger;
-        }
-        */
+       
         public IActionResult Index()
         {
             return View(Repository.AllStudents);
@@ -38,8 +32,8 @@ namespace crud_application.Controllers
         [HttpPost]
         public IActionResult Create(Student student)
         {
-            Repository.Create(student);
-            return View("Thanks", student);
+                Repository.Create(student);
+                return View("Thanks", student);
         }
 
         public IActionResult Update(string empname)
@@ -52,29 +46,26 @@ namespace crud_application.Controllers
         [HttpPost]
         public IActionResult Update(Student student, string empname)
         {
-            
-            Repository.AllStudents.Where(e => e.Name == empname).FirstOrDefault().Age = student.Age;
-            Repository.AllStudents.Where(e => e.Name == empname).FirstOrDefault().Year = student.Year;
-            Repository.AllStudents.Where(e => e.Name == empname).FirstOrDefault().Major = student.Major;
-            Repository.AllStudents.Where(e => e.Name == empname).FirstOrDefault().Sex = student.Sex;
-            Repository.AllStudents.Where(e => e.Name == empname).FirstOrDefault().Name = student.Name;
 
-            return RedirectToAction("Index");
+                Repository.AllStudents.Where(e => e.Name == empname).FirstOrDefault().Name = student.Name;
+                Repository.AllStudents.Where(e => e.Name == empname).FirstOrDefault().Age = student.Age;
+                Repository.AllStudents.Where(e => e.Name == empname).FirstOrDefault().Year = student.Year;
+                Repository.AllStudents.Where(e => e.Name == empname).FirstOrDefault().Major = student.Major;
+                Repository.AllStudents.Where(e => e.Name == empname).FirstOrDefault().Sex = student.Sex;
+                Repository.AllStudents.Where(e => e.Name == empname).FirstOrDefault().Subject = student.Subject;
+
+                return RedirectToAction("Index");
+        
         }
 
         [HttpPost]
-        public IActionResult Delete(string empname)
-        {
-            Student student = Repository.AllStudents.Where(e => e.Name == empname).FirstOrDefault();
-            Repository.Delete(student);
-            return RedirectToAction("Index");
-        }
+    public IActionResult Delete(string empname)
+    {
+        Student student = Repository.AllStudents.Where(e => e.Name == empname).FirstOrDefault();
+        Repository.Delete(student);
+        return RedirectToAction("Index");
+    }
 
-
-        /*[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }*/
+   
     }
 }
--- crud-application/crud-application/Models/Student.cs
+++ crud-application/crud-application/Models/Student.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,10 +8,17 @@ namespace crud_application.Models
 {
     public class Student
     {
+        
+        public string Subject { get; set; }
+
         public string Name { get; set; }
+
         public int Age { get; set; }
+
         public int Year { get; set; }
+
         public string Major { get; set; }
+
         public Char Sex { get; set; }
     }
 }
